9ae07adac0ece44f63eb9b2b17494cda7d5806e9
NexControl <nexcontrolau@gmail.com>
2026-07-11 16:59:58 -0500

    Create method for connect plc

---
+++ ControlLevelJets/Services/S7ConnectionService.cs
@@ -0,0 +1,22 @@
+﻿using S7.Net;
+
+namespace ControlLevelJets.Services;
+
+public partial class S7ConnectionService : ObservableObject, IS7ConnectionService
+{
+    private const string PlcIpAddress = "192.168.20.50";
+    private const CpuType PlcCpuType = CpuType.S71200;
+    private const short PlcRackId = 0;
+    private const short PlcSlotId = 1;
+
+    public Plc? PlcStation;
+
+    public async Task ConnectS7Station()
+    {
+
+        PlcStation = new Plc(PlcCpuType, PlcIpAddress, PlcRackId, PlcSlotId);
+
+        await PlcStation.OpenAsync();
+
+    }
+}
