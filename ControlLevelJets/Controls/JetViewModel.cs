68c4c101c3853913834ea76551ca3957a34d6951
NexControl <nexcontrolau@gmail.com>
2026-07-19 22:04:25 -0500

    Create OnModelChanged for update properties that bind in UI

--- ControlLevelJets/Controls/JetViewModel.cs
+++ ControlLevelJets/Controls/JetViewModel.cs
@@ -1,5 +1,6 @@
 using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
+using System.Diagnostics;
 using ControlLevelJets.Presentation.Interfaces;
 using ControlLevelJets.Services;
 
@@ -8,13 +9,7 @@ namespace ControlLevelJets.Controls;
 public partial class JetViewModel : ObservableObject
 {
     [ObservableProperty] private string _name;
-
     [ObservableProperty] private short _desiredLiters;
-
-    [ObservableProperty] private double _totalLiters;
-
-    [ObservableProperty] private bool _valveState;
-
     [ObservableProperty] private string _valveStatus;
 
     // PLC Addresses
@@ -22,21 +17,25 @@ public partial class JetViewModel : ObservableObject
     [ObservableProperty] private string _resetValuesAddress = string.Empty;
 
     // Properties for data binding from Read Current Values
-    [ObservableProperty] private short _setpointLiters;
+    public short SetpointL => Model.SetpointLiters;
+    public float TotalLiters => Model.CurrentLiters;
+    public bool ValveState => Model.ValveStatus;
 
     private readonly IJetActions _jetActions;
+    private readonly JetModel Model;
 
-    public JetViewModel(IJetActions jetActions)
+    public JetViewModel(IJetActions jetActions, JetModel model)
     {
 
         _jetActions = jetActions;
+        Model = model;
 
         // Initialize properties values
-        //Name = string.Empty;
-        //DesiredLiters = 0;
-        //TotalLiters = 0;
-        //ValveState = false;
-        //ValveStatus = ValveState ? "Abierta" : "Cerrada";
+        Name = string.Empty;
+        DesiredLiters = 0;
+        ValveStatus = ValveState ? "Abierta" : "Cerrada";
+
+        Model.PropertyChanged += OnModelChanged;
 
     }
 
@@ -45,4 +44,27 @@ public partial class JetViewModel : ObservableObject
 
     [RelayCommand]
     private void ResetCurrentValues() => _jetActions.ResetCurrentValues(this);
+
+    public void OnModelChanged(object? sender, PropertyChangedEventArgs e)
+    {
+        if (e.PropertyName == nameof(Model.SetpointLiters))
+        {
+            OnPropertyChanged(nameof(SetpointL));
+            Debug.WriteLine($"Value of setpointL {SetpointL}");
+        }
+
+        if (e.PropertyName == nameof(Model.CurrentLiters))
+        {
+            OnPropertyChanged(nameof(TotalLiters));
+            Debug.WriteLine($"Value of totalLiters {TotalLiters}");
+        }
+
+        if (e.PropertyName == nameof(Model.ValveStatus))
+        {
+            OnPropertyChanged(nameof(ValveState));
+            Debug.WriteLine($"Value of valveState {ValveState}");
+            ValveStatus = ValveState ? "Abierta" : "Cerrada";
+        }
+
+    }
 }
